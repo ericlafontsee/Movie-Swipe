f3016240a80199b4a221576b03051641f39021bf
Eric LaFontsee <elafontsee@gmail.com>
2021-01-10 13:17:27 -0600

    Set up API GET route for TMDB movie search

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-
 var genreCategory = [{
         genre: "Action",
         genreID: 28
@@ -58,10 +57,7 @@ var genreCategory = [{
     }
 ]
 
-// Queries movies based on genre selection
 function movieGenre() {
-    // event.preventDefault();
-    // selectedGenre = $("#genreDropDown").children("option:selected").val();
     selectedGenre = 80;
     console.log(selectedGenre);
     var genreID = parseInt(selectedGenre);
@@ -70,77 +66,14 @@ function movieGenre() {
 
     axios
       .get(queryGenre)
-
       .then(function(res) {
-        console.log(res.data);
+        let results = res.data.results;
+        results.forEach(res => {
+            console.log(res.title);
+        })
       });
     
     }
-    // axios.get({
-    //     url: queryGenre,
-    //     method: "GET"
-    // }).then(function(response) {
-        // var st = Math.floor(Math.random() * 14);
-        // var end = st + 5;
-        // for (var i = st; i < end; i++) {
-        //     // console.log(response.results[i].title);
-        //     var poster = response.results[i].poster_path;
-        //     var imgURL = "https://image.tmdb.org/t/p/w500" + poster;
-        //     var genreID = response.results[i].genre_ids;
-
-        //     var newDiv = $("<div>");
-        //     var br = $("<br>");
-        //     $("#movieInfoHere").append(newDiv);
-        //     $("#movieInfoHere").append(br);
-        //     var movieTitle = $("<h2>").text(response.results[i].title);
-        //     newDiv.append(movieTitle);
-        //     var movieSummary = $("<p>").text(response.results[i].overview);
-        //     newDiv.append(movieSummary);
-        //     newDiv.append(br);
-        //     var poster = $("<img>").attr("src", imgURL);
-        //     $("#moviePosterHere").append(poster);
-        //     $("#moviePosterHere").append(br);
-
-        // }
-//         console.log(response);
-//     });
-// }
 
 movieGenre();
 
-// // Queries trending movies in 2020 and randomly selects out of the list of arrays
-// $("#movieRandom").on("click", function() {
-//     event.preventDefault();
-//     $("#moviePosterHere").empty();
-//     $("#movieInfoHere").empty();
-//     $("#movieMaster").show();
-//     $("#movieMaster").children().show();
-//     musicGenre();
-//     var APIKey = "1a0244fad68dbfa1e242e232ce4a493c"; //TMDB api
-//     var queryTrends = "https://api.themoviedb.org/3/trending/all/day?api_key=1a0244fad68dbfa1e242e232ce4a493c&language=en-US&primary_release_year=2020&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
-//     $.ajax({
-//         url: queryTrends,
-//         method: "GET"
-//     }).then(function(response) {
-//         var st = Math.floor(Math.random() * 18);
-//         var end = st + 5;
-//         for (var i = st; i < end; i++) {
-//             // console.log(response.results[i].title);
-//             var poster = response.results[i].poster_path;
-//             var imgURL = "https://image.tmdb.org/t/p/w500" + poster;
-//             var genreID = response.results[i].genre_ids;
-
-//             var newDiv = $("<div>");
-//             $("#movieInfoHere").append(newDiv);
-
-//             var movieTitle = $("<h2>").text(response.results[i].title);
-//             newDiv.append(movieTitle);
-//             var movieSummary = $("<p>").text(response.results[i].overview);
-//             newDiv.append(movieSummary);
-//             var poster = $("<img>").attr("src", imgURL);
-//             $("#moviePosterHere").append(poster);
-
-//         }
-//         console.log(response);
-//     });
-// });
