4f4e575e96f9c3909b469a382f62dd33ab155ab3
Chris Reed <cr31293@gmail.com>
2021-01-15 15:00:59 -0500

    logo and beginning of header added

---
+++ movie-match/src/components/header/header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import ToolBar from '@material-ui/core/ToolBar';
+
+import logo from '../../assets/Header.svg';
+
+ const useStyles = makeStyles((theme) => ({
+     logo: {
+         height: "8em",
+         backgroundColor: "main",
+         
+     },
+     logoContainer: {
+        padding: 0
+     },
+     appbar: {
+         backgroundColor: "#EF4B4B",
+         marginBottom: "5em",
+     }
+ }));
+
+export default function Header() {
+    const classes = useStyles();
+    const theme = useTheme();
+    return (
+        <>
+            <AppBar className={classes.appbar}>
+                <ToolBar disableGutters>
+                    <Button className={classes.logoContainer}>
+                        <img alt="site logo" src={logo} className={classes.logo} />
+                    </Button>
+                </ToolBar>
+            </AppBar>
+        </>
+    )
+};
+
