a0749e15d72acb63a35c9b7fea7c5cd81ebe497f
Eric LaFontsee <elafontsee@gmail.com>
2021-01-18 18:41:17 -0600

    Setup get route for mymatches and mapped through them to display to a card

--- movie-match/src/pages/mymatches.js
+++ movie-match/src/pages/mymatches.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
+import SavedCard from "../components/Savedcard";
 import API from "../utils/API";
 
 function Saved() {
   const [savedMovies, setSavedMovies] = useState([]);
-
+console.log(savedMovies);
   useEffect(() => {
     API.getMatches()
       .then((res) => {
@@ -15,9 +16,8 @@ function Saved() {
 
   return (
     <div className="saved">
-  
         <div>
-        {/* {savedMovies.map((movie, index ) => <h2 key={index} {...movie} />)} */}
+        {savedMovies.map((movie, index ) => <SavedCard key={index} {...movie} /> )}
         </div>
     </div>
   );
