d431b25e11320b38d1192deb71d62d56f6712f42
Eric LaFontsee <elafontsee@gmail.com>
2021-01-31 13:24:12 -0600

    Set up basic onclick for each movie card that darkens the whole page

--- movie-match/src/components/MyMatches/MyMatches.js
+++ movie-match/src/components/MyMatches/MyMatches.js
@@ -1,13 +1,31 @@
-import { render } from 'react-dom'
-import React, { useState, useEffect, useMemo } from 'react';
-import { useTransition, a } from 'react-spring';
-import shuffle from 'lodash/shuffle';
-import useMeasure from './useMeasure';
-import useMedia from './useMedia';
-import './styles.css';
+import React, { useState, useEffect, useMemo } from "react";
+import { useTransition, a } from "react-spring";
+import useMeasure from "./useMeasure";
+import useMedia from "./useMedia";
+import "./styles.css";
 import API from "../../utils/API";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function MyMatches() {
+  const useStyles = makeStyles((theme) => ({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      backgroundColor: "rgba(0, 0, 0, 0.85)"
+    }
+  }));
+
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
   const [savedMovies, setSavedMovies] = useState([]);
   useEffect(() => {
     API.getMatches()
@@ -17,26 +35,41 @@ export default function MyMatches() {
       .catch((err) => console.log(err));
   }, []);
 
-savedMovies.map(movie => {
-     movie.css = 'url(https://www.themoviedb.org/t/p/w600_and_h900_bestv2' + movie.posterImage + ')';
-     movie.height = 900;
-  })
+  savedMovies.map((movie) => {
+    movie.css =
+      "url(https://www.themoviedb.org/t/p/w600_and_h900_bestv2" +
+      movie.posterImage +
+      ")";
+    movie.height = 900;
+  });
 
   // Hook1: Tie media queries to the number of columns
-  const columns = useMedia(['(min-width: 600px)', '(min-width: 600px)', '(min-width: 600px)', '(min-width: 600px)'], [5, 4, 3, 2], 1)
+  const columns = useMedia(
+    [
+      "(min-width: 600px)",
+      "(min-width: 600px)",
+      "(min-width: 600px)",
+      "(min-width: 600px)"
+    ],
+    [5, 4, 3, 2],
+    1
+  );
 
   // Hook2: Measure the width of the container element
-  const [bind, { width }] = useMeasure()
+  const [bind, { width }] = useMeasure();
 
   // Hook5: Form a grid of stacked items using width & columns we got from hooks 1 & 2
   const [heights, gridItems] = useMemo(() => {
-    let heights = new Array(columns).fill(0) // Each column gets a height starting with zero
+    let heights = new Array(columns).fill(0); // Each column gets a height starting with zero
     let gridItems = savedMovies.map((child, i) => {
-      const column = heights.indexOf(Math.min(...heights)) // Basic masonry-grid placing, puts tile into the smallest column using Math.min
-      const xy = [(width / columns) * column, (heights[column] += child.height / 2) - child.height / 2] // X = container width / number of columns * column index, Y = it's just the height of the current column
-      return { ...child, xy, width: width / columns, height: child.height / 2 }
-    })
-    return [heights, gridItems]
+      const column = heights.indexOf(Math.min(...heights)); // Basic masonry-grid placing, puts tile into the smallest column using Math.min
+      const xy = [
+        (width / columns) * column,
+        (heights[column] += child.height / 2) - child.height / 2
+      ]; // X = container width / number of columns * column index, Y = it's just the height of the current column
+      return { ...child, xy, width: width / columns, height: child.height / 2 };
+    });
+    return [heights, gridItems];
   }, [columns, savedMovies, width]);
 
   // Hook6: Turn the static grid values into animated transitions, any addition, removal or change will be animated
@@ -46,16 +79,38 @@ savedMovies.map(movie => {
     update: ({ xy, width, height }) => ({ xy, width, height }),
     leave: { height: 0, opacity: 0 },
     config: { mass: 5, tension: 500, friction: 100 },
-    trail: 25,
-  })
+    trail: 25
+  });
   // Render the grid
   return (
     <div {...bind} class="list" style={{ height: Math.max(...heights) }}>
       {transitions.map(({ item, props: { xy, ...rest }, key }) => (
-        <a.div key={key} style={{ transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`), ...rest }}>
-          <div className="movie" style={{ backgroundImage: item.css }} />
+        <a.div
+          key={key}
+          style={{
+            transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`),
+            ...rest
+          }}
+        >
+          <div style={{ backgroundImage: item.css }}>
+            <Button
+              style={{ width: "100%", height: "100%", opacity: "0" }}
+              variant="outlined"
+              color="primary"
+              onClick={handleToggle(ev)}
+            />
+          </div>
         </a.div>
       ))}
+
+      <Backdrop
+        className={classes.backdrop}
+        open={open}
+        onClick={handleClose}
+      >
+
+
+      </Backdrop>
     </div>
-  )
+  );
 }
