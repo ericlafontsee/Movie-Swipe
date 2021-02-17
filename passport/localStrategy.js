aa1c429fe58a556994a7ad9ed47ae96b38d247c1
Chris Reed <cr31293@gmail.com>
2021-02-16 19:51:09 -0500

    getting a 200 response on sign in now. Need to add redirect

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -5,7 +5,7 @@ const LocalStrategy = require('passport-local').Strategy;
 
 const strategy = new LocalStrategy(
 	{
-		usernameField: 'username' // not necessary, DEFAULT
+		usernameField: 'email' // not necessary, DEFAULT
 	},
 	function(email, password, done) {
 		User.findOne({ email: email }, (err, user) => {
@@ -13,7 +13,7 @@ const strategy = new LocalStrategy(
 				return done(err)
 			}
 			if (!user) {
-				return done(null, false, { message: 'Incorrect username' })
+				return done(null, false, { message: 'Incorrect email' })
 			}
 			if (!user.checkPassword(password)) {
 				return done(null, false, { message: 'Incorrect password' })
